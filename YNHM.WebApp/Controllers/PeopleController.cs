abf27e8f5b06aa4399d4ffa3c224c0ba031acd02
DimitraK92 <degraded_rebuilt@tutanota.com>
2021-07-28 15:22:30 +0300

    Dimitra: 1) Controllers created: -PeopleController, -HousesController, -MessagesController, -PasswordController, -ProfileController
    2)Views created:
    -People
    -Houses
    -Messages
    -Password
    -Profile
    
    3)Updated navigation links inside Index (of HomePage), MyAccount/dashboard EditProfile.
    
    In general,  all views need to be adapted to our project. They contain simply the same navigation bar and some basic functionality inside nested navigation links plus some altered titles, but no further adjustments (to correspond to our project).

---
+++ YNHM.WebApp/Controllers/PeopleController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace YNHM.WebApp.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // GET: People
+        public ActionResult People()
+        {
+            return View();
+        }
+    }
+}
