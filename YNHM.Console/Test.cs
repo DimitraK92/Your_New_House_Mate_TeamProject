b7dd0ea86f9d317809cfc166d5fa360821879254
evabasa <79225477+evabasa@users.noreply.github.com>
2021-08-09 00:47:25 +0300

    Test html with css +prwti skepsi test se consola

---
+++ YNHM.Console/Test.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YNHM.Console
+{
+    class Test
+    {
+        //primary key
+        public int TestId;
+
+        //foreign key
+        public int PersonId;
+
+        //properties
+        
+        public bool Smoking;
+        public bool PetOwner;
+        public MusicGenre musicGenre;
+     }
+
+    public enum MusicGenre
+    {
+        MetalRock,
+        Pop,
+        HipHop,
+        Reggae
+    }
+}
