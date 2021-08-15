300fcf4a8bd3a09d92be0a94b4c4907c6654fb97
Voskakis <constantinosvoskakis@hotmail.gr>
2021-08-15 12:56:03 +0300

    CREATE: BaseModel, Person, ConnectionString, ApplicationDbContext, UPDATE: Configuration to hold connection string

---
+++ YNHM.Database/Models/Person.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using YNHM.Database.Models.Base;
+
+namespace YNHM.Database.Models
+{
+    public class Person : BaseModel
+    {
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string Lastname { get; set; }
+
+        public int Age { get; set; }
+
+        [Display(Name = "Photograph URL")]
+        public string PhotoUrl { get; set; }
+
+        [Display(Name = "Match Percentage")]
+        public int MatchPercent { get; set; }
+
+        [Display(Name = "Phone Number")]
+        public string Phone { get; set; }
+
+        public string Email { get; set; }
+
+        [Display(Name = "Facebook Profile")]
+        public string Facebook { get; set; }
+
+        public string Description { get; set; }
+    }
+}
