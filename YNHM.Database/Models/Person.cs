2930690897312738a8517468d1980d19e40cc267
Voskakis <constantinosvoskakis@hotmail.gr>
2021-08-15 15:40:59 +0300

    Update HomePageController connected with dbcontext

--- YNHM.Database/Models/Person.cs
+++ YNHM.Database/Models/Person.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using YNHM.Database.Models.Base;
 
 namespace YNHM.Database.Models
@@ -28,5 +29,18 @@ namespace YNHM.Database.Models
         public string Facebook { get; set; }
 
         public string Description { get; set; }
+
+        /// <summary>
+        /// Used when the Description property of a Person object is too long.
+        /// </summary>
+        /// <returns>The first maxSize characters of the Description property, or less, if they are less than maxSize.</returns>
+        public string TrimDescription(int maxSize)
+        {
+            if (Description == "" || Description.Length <= maxSize)
+            {
+                return Description;
+            }
+            return Description.Substring(0, maxSize);
+        }
     }
 }
