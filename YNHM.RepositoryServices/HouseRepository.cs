fd70294edbacbecf71cbbe7cb4c7d43a54f82d1e
DimitraK92 <degraded_rebuilt@tutanota.com>
2021-08-16 17:02:14 +0300

    Dimitra: House + Photo Repositories

---
+++ YNHM.RepositoryServices/HouseRepository.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YNHM.Database;
+using YNHM.Database.Models;
+
+namespace YNHM.RepositoryServices
+{
+    class HouseRepository
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public List<House> GetAll()
+        {
+            return db.Houses.ToList();          
+        }
+
+        public House GetById(int? id)
+        {
+            return db.Houses.Find(id);
+        }
+
+        public void Create(House house, IEnumerable<int?> SelectedPhotoIds)
+        {
+            Attach(house);
+            house.Photos.Clear();
+            db.SaveChanges();
+
+            if (!(SelectedPhotoIds is null))
+            {
+                foreach (var id in SelectedPhotoIds)
+                {
+                    Photo photo = db.Photos.Find(id);
+                    if (photo != null)
+                    {
+                        house.Photos.Add(photo);
+                    }
+                }
+            }
+            db.Entry(house).State = EntityState.Added;
+            db.SaveChanges();
+        }
+
+        public void Edit(House house, IEnumerable<int?> SelectedPhotoIds)
+        {
+            Attach(house);
+            house.Photos.Clear();
+            db.SaveChanges();
+
+            if (!(SelectedPhotoIds is null))
+            {
+                foreach (var id in SelectedPhotoIds)
+                {
+                    Photo photo = db.Photos.Find(id);
+                    if (photo != null)
+                    {
+                        house.Photos.Add(photo);
+                    }
+                }
+            }
+            db.Entry(house).State = EntityState.Modified;
+            db.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            House house = db.Houses.Find(id);
+            house.Photos.Clear();
+
+            db.Entry(house).State = EntityState.Deleted;
+            db.SaveChanges();
+        }
+
+        public void Attach(House house)
+        {
+            db.Houses.Attach(house);
+            db.Entry(house).Collection("Photos").Load();
+        }
+
+        private bool disposed = false;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    db.Dispose();
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ YNHM.RepositoryServices/PhotoRepository.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YNHM.Database;
+using YNHM.Database.Models;
+
+namespace YNHM.RepositoryServices
+{
+    class PhotoRepository
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public List<Photo> GetAll()
+        {
+            return db.Photos.ToList();
+        }
+
+        public Photo GetById(int? id)
+        {
+            return db.Photos.Find(id);
+        }
+
+        public void Create(Photo photo)
+        {
+           
+            db.Entry(photo).State = EntityState.Added;
+            db.SaveChanges();
+        }
+
+        public void Edit(Photo photo)
+        {
+           
+            db.Entry(photo).State = EntityState.Modified;
+            db.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            Photo photo = db.Photos.Find(id);
+            if (!(photo is null))
+            {
+                db.Entry(photo).State = EntityState.Deleted;
+                db.SaveChanges();
+            }
+            else
+            {
+                throw new Exception("The photo could not be deleted.");
+            }
+        }
+
+        private bool disposed = false;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    db.Dispose();
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
