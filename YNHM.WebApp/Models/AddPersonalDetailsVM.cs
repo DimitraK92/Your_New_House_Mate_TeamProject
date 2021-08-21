5c92b5586c0cf83708542118e421758a39c60997
Vassilis KG <vassilis.kotsman@gmail.com>
2021-08-21 22:58:59 +0300

    - Extended User with Person - Added Account/AddAdditional Info View - Added Manage/UserDetails View

---
+++ YNHM.WebApp/Models/AddPersonalDetailsVM.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace YNHM.WebApp.Models
+{
+    public class AddPersonalDetailsVM
+    {
+        [Display(Name = "First Name")]
+        [MinLength(2,ErrorMessage ="First name should have at least 2 characters.")]
+        [MaxLength(15,ErrorMessage ="First name should have up to 15 characters")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [MinLength(2, ErrorMessage = "Last Name should have at least 2 characters.")]
+        [MaxLength(15, ErrorMessage = "Last name should have up to 15 characters")]
+        public string LastName { get; set; }
+
+        public string Description { get; set; }
+        public int Age { get; set; }
+
+        [Display(Name = "Phone Number")]
+        [StringLength(10,ErrorMessage ="Please enter a 10 digit phone number.",ErrorMessageResourceName = null,ErrorMessageResourceType = null,MinimumLength = 10)]
+        public string Phone { get; set; }
+
+        [Display(Name = "Facebook Profile")]
+        public string Facebook { get; set; }
+
+        public bool ContinueLater { get; set; }
+
+    }
+}
--- YNHM.WebApp/Models/PersonDetailsVM.cs
+++ YNHM.WebApp/Models/PersonDetailsVM.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using YNHM.Database.Models;
 
 namespace YNHM.WebApp.Models
 {
     public class PersonDetailsVM
     {
+   
+
+
+        public Person Person { get; set; }
+
+        public PersonDetailsVM(Person person)
+        {
+            Person = person;
+        }
+
 
         [Display(Name = "First Name")]
         public string FirstName { get; set; }
@@ -21,7 +32,7 @@ namespace YNHM.WebApp.Models
         public string Description { get; set; }
 
         public string PhotoUrl { get; set; }
-    
+
         [Display(Name = "Phone Number")]
         public string Phone { get; set; }
 
