b5771743b34f40708b835c328e54b45dbc6e7cc0
Vassilis G. Kotsmanidis <vassilis.kotsman@gmail.com>
2021-08-24 12:46:31 +0300

    Changed DB name to HouseMate

--- YNHM.Database/ApplicationDbContext.cs
+++ YNHM.Database/ApplicationDbContext.cs
@@ -11,12 +11,12 @@ namespace YNHM.Database
         public DbSet<Person> People { get; set; }
         public DbSet<House> Houses { get; set; }
         public DbSet<Photo> Photos { get; set; }
-        public ApplicationDbContext() : base(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ONOMA;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False", throwIfV1Schema: false)
+        public ApplicationDbContext() : base(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=HouseMate;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False", throwIfV1Schema: false)
         {
             this.Configuration.LazyLoadingEnabled = true;
             this.Configuration.ProxyCreationEnabled = true;
         }
-        public ApplicationDbContext(string connectionString= "ONOMA") : base(connectionString)
+        public ApplicationDbContext(string connectionString= "HouseMate") : base(connectionString)
         {
             this.Configuration.LazyLoadingEnabled = true;
             this.Configuration.ProxyCreationEnabled = true;
