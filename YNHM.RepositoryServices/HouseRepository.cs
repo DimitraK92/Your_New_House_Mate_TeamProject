ee7e414f9c7e6862fd9a5628d93545155952cc57
DimitraK92 <degraded_rebuilt@tutanota.com>
2021-08-29 16:20:33 +0300

    fixed BUG in admin CRUD (edited House Model, Admin House Controller, admin house index/delete/edit/details views

--- YNHM.RepositoryServices/HouseRepository.cs
+++ YNHM.RepositoryServices/HouseRepository.cs
@@ -46,7 +46,8 @@ namespace YNHM.RepositoryServices
 
         public void Edit(House house, IEnumerable<int?> SelectedPhotoIds)
         {
-            Attach(house);
+            db.Houses.Attach(house);
+            db.Entry(house).Collection("Photos").Load();
             house.Photos.Clear();
             db.SaveChanges();
 
@@ -60,6 +61,7 @@ namespace YNHM.RepositoryServices
                         house.Photos.Add(photo);
                     }
                 }
+                db.SaveChanges();
             }
             db.Entry(house).State = EntityState.Modified;
             db.SaveChanges();
