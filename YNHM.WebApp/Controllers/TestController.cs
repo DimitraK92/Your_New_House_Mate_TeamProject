4aeab88958a25e1e0a5c743e076a3574e69e34b1
Vassilis G. Kotsmanidis <vassilis.kotsman@gmail.com>
2021-09-09 15:13:04 +0300

    Close enough

--- YNHM.WebApp/Controllers/TestController.cs
+++ YNHM.WebApp/Controllers/TestController.cs
@@ -14,7 +14,7 @@ using YNHM.WebApp.Models;
 
 namespace YNHM.WebApp.Controllers
 {
-    [Authorize(Roles = "Admin, HouseSeeker")]
+    [Authorize(Roles = "Admin, Roomie")]
     public class TestController : Controller
     {
         private readonly ApplicationDbContext db = new ApplicationDbContext();
@@ -54,7 +54,6 @@ namespace YNHM.WebApp.Controllers
         {
             var roomie = GetRoomie();
             Test test = roomie.Test;
-            AnswerQuestionsVM vm = new AnswerQuestionsVM();
 
             ViewBag.Answers = new List<SelectListItem>()
             {
@@ -63,16 +62,66 @@ namespace YNHM.WebApp.Controllers
                 new SelectListItem { Text = "No"}
             };
 
-            ViewBag.TestQuestions = test.Questions.ToList();
+            List<Question> Questions = new List<Question>()
+            {
+                //Housework
+                new Question() { Text = "Does cleanliness matter in general?" },
+                new Question() { Text = "Are you bothered by a sink full with plates?" },
+                new Question() { Text = "Do you mind the house being dusty?" },
+                new Question() { Text = "Is cleaning the toilet every day important to you?" },
 
-            return View(test);
+                //Noise
+                new Question() { Text = "Does noise matter in general?" },
+                new Question() { Text = "Do you listen to music loud?" },
+                new Question() { Text = "Do you value quiet in the house" },
+                new Question() { Text = "Will you be with playing musical instruments?" },
+
+                //Food
+                new Question() { Text = "Is a vegan or vegetarian diet important to you?" },
+                new Question() { Text = "Do you cook every day?" },
+                new Question() { Text = "Is keeping your own groceries separate important?" },
+                new Question() { Text = "Is eating with your housemates important?" },
+
+                //Animals
+                new Question() { Text = "Do you like animal companions?" },
+                new Question() { Text = "Will you mind caring for the housemate's animal companions, if they are absent?" },
+
+                //Friends
+                new Question() { Text = "Doo you mind friends coming over?" },
+                new Question() { Text = "Do you mind friends or significant others staying overnight?" },
+                new Question() { Text = "Does hanging out with your housemates matter?" },
+
+                //Smoking
+                new Question() { Text = "Do you mind others smoking in the house?" },
+                new Question() { Text = "Do you mind others smoking weed in the house?" }
+            };
+            return View(Questions);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult TakeTest([Bind(Include = "TestId,Name")]Test test, IEnumerable<Question> TestQuestions)
+        public ActionResult TakeTest(List<Question> questions)
         {
-            return View(test);
+            var roomie = GetRoomie();
+            var test = roomie.Test;
+
+            if (ModelState.IsValid)
+            {
+                foreach (var tq in test.Questions)
+                {
+                    foreach (var q in questions)
+                    {
+                        tq.Answer = q.Answer;
+                    }
+                }
+
+                db.Entry(test).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index", "Manage");
+            }
+            List<Question> Questions = test.Questions.ToList();
+
+            return View(Questions);
         }
 
 
@@ -87,12 +136,45 @@ namespace YNHM.WebApp.Controllers
                 return RedirectToAction("TakeTest", "Test");
             }
 
-            Test test = new Test() 
+            Test test = new Test()
             {
                 Roomie = roomie,
                 Name = $"{roomie.FirstName} {roomie.LastName}",
-                Questions = new List<Question>()
+                Questions =new List<Question>()
+                {
+                    //Housework
+                    new Question() { Text = "Does cleanliness matter in general?" },
+                    new Question() { Text = "Are you bothered by a sink full with plates?" },
+                    new Question() { Text = "Do you mind the house being dusty?" },
+                    new Question() { Text = "Is cleaning the toilet every day important to you?" },
+
+                    //Noise
+                    new Question() { Text = "Does noise matter in general?" },
+                    new Question() { Text = "Do you listen to music loud?" },
+                    new Question() { Text = "Do you value quiet in the house" },
+                    new Question() { Text = "Will you be with playing musical instruments?" },
+
+                    //Food
+                    new Question() { Text = "Is a vegan or vegetarian diet important to you?" },
+                    new Question() { Text = "Do you cook every day?" },
+                    new Question() { Text = "Is keeping your own groceries separate important?" },
+                    new Question() { Text = "Is eating with your housemates important?" },
+
+                    //Animals
+                    new Question() { Text = "Do you like animal companions?" },
+                    new Question() { Text = "Will you mind caring for the housemate's animal companions, if they are absent?" },
+
+                    //Friends
+                    new Question() { Text = "Doo you mind friends coming over?" },
+                    new Question() { Text = "Do you mind friends or significant others staying overnight?" },
+                    new Question() { Text = "Does hanging out with your housemates matter?" },
+
+                    //Smoking
+                    new Question() { Text = "Do you mind others smoking in the house?" },
+                    new Question() { Text = "Do you mind others smoking weed in the house?" }
+                }
             };
+
 
             if (!ModelState.IsValid)
             {
@@ -102,6 +184,7 @@ namespace YNHM.WebApp.Controllers
             roomie.Test = test;
             db.Entry(roomie).State = EntityState.Modified;
             db.Tests.Add(test);
+            db.SaveChanges();
             return RedirectToAction("TakeTest", "Test");
         }
 
